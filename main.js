27054cfe1610f9a505fd84d25e5188cd96044d67
abroraxmedov04 <abrordevaxmedov@gmail.com>
2023-10-06 16:39:44 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,133 @@
+// My first line of code in Js
+
+// ! begin 1
+// let a = prompt("enter the number");
+// const P = 4 * a;
+// alert(P);
+
+// !begin2
+// let a = prompt("enter the number");
+// let S = a**2
+// alert(S)
+
+// !begin 3
+// var a = prompt("enter the first number")
+// var b = prompt("enter the second number")
+// const S = a*b;
+// const P = 2*(a+b);
+// alert(`The area is ${S} and Perimeter is ${P}`);
+
+// !begin 4
+// let d = prompt("enter the number");
+// const p = 3.14;
+// const L = p*d;
+// alert(`${L}`)
+
+// !begin 5
+// let a = prompt("Enter any number");
+// const V = a ** 3;
+// const S = 6 *(a**2);
+// alert (`space: ${V}, Surface Area :${S}`);'
+
+// !begin 6
+// let a = prompt("write the first number")
+// let b = prompt("write the second number")
+// let c = prompt("write the third number")
+// const V = a*b*c;
+// const S = 2*(a*b+b*c+a*c);
+// alert(`Volume:${V}\nSurfaceArea:${S}`);
+
+// !begin 7
+// let R = prompt("write the number");
+// const p = 3.14;
+// const L = 2*p*R;
+// const S = p*(R**2);
+// alert(`Circumference: ${L}\nAnd Surface Area is ${S}`);
+
+// !begiin 8
+// var a = prompt("enter the first number")
+// var b = prompt("enter the second number")
+// const S = (a+b)/2;
+// alert(`the average of two numbers is ${S}`);
+
+// !integer 1
+// let l = prompt("Wtite the number");
+// const m = l/100;
+// alert(`your width in metres is ${m}`);
+
+// !integer 2
+// let M = prompt("Wtite the number");
+// const kg = M*1000;
+// alert(kg)
+
+// !integer 3
+// let kb = prompt("Wtite the number");
+// const MB = kb/1024;
+// alert(`${MB}`);
+
+// !integer 4
+// let a = prompt("enter thr number");
+// let b = prompt("enter the number");
+// if (a > b) {
+//   const P = a / b;
+//   alert(`the result is ${P}`);
+// } else {
+//   alert("incorrect");
+// }
+
+// !integer 5
+// let a = prompt("enter the number");
+// let b = prompt("enter the number");
+// if (a > b) {
+//   const P = a%b;
+//   alert (`The remainder is ${P}`) ;}else{
+//     alert ("Incorrect input");
+// }
+
+// !integer 6
+// let x = prompt("write the numbetr");
+// const a = x % 10;
+// const b = x / 10 - a / 10;
+// alert (`first digit :${b}, second digt: ${a} `);
+
+// !integer 7
+// let a = prompt("enter the number")
+// let b = prompt("enter the number")
+// const result = (a)+(b);
+// alert(result);
+
+// !integer 8
+// let x = prompt("write the numbetr");
+// const a = x % 10;
+// const b = x / 10 - a / 10;
+// alert (`first digit :${a}, second digt: ${b} `);
+
+// !integer 19
+// let N =prompt("any number")
+// const m = 60;
+// const result = N/m;
+// alert(result.toFixed());
+
+// !integer 20
+// let N =prompt("any number")
+// const m = 3600;
+// const result = N/m;
+// alert(result.toFixed());
+
+// !integer 22
+// let N = prompt("any number");
+// const m = 60;
+// const s = 3600;
+// const resultFirst = N / m;
+// const resultSecond = N - m * s;
+// alert(
+//   `${Math.floor(resultFirst)} hour and ${Math.round(resultSecond)} seconds`
+// );
+
+// !integer 23
+// let n=Number(prompt('any number'));
+// const a = n/3600 /* minuts*/
+// const b = n/60 /* hours*/
+// alert(`${n} minutes are equal to ${a} hours`);
+
+
